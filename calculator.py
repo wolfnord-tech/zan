daedcd4b8b3e06a4f0bc8cfe82c3f1c7560a92b6
WolfNord <wolfnord.tech@yandex.ru>
2025-12-11 11:27:44 +0300

    Swapped the functions

--- calculator.py
+++ calculator.py
@@ -1,12 +1,13 @@
 import math
-def get_area(a, b):
- return a * b
 def get_hypotenuse(a, b):
  return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
+def get_area(a, b):
+ return a * b
 if __name__ == "__main__":
  print("Введите a:")
  a = int(input())
  print("Введите b:")
  b = int(input())
- print("c =", get_hypotenuse(a,b))
- print("S =", get_area(a,b))
+ print("c =", get_hypotenuse(a, b))
+ print("S =", get_area(a, b))
+ 
