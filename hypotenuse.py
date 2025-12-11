15f54f805a58a1caca5f1ce1857a812f06020c2d
WolfNord <wolfnord.tech@yandex.ru>
2025-12-11 11:11:07 +0300

    Added new features for hypotenuse.py

--- hypotenuse.py
+++ hypotenuse.py
@@ -1,4 +1,6 @@
 import math
+def get_area(a, b):
+ return a * b
 def get_hypotenuse(a, b):
  return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 if __name__ == "__main__":
@@ -7,3 +9,4 @@ if __name__ == "__main__":
  print("Введите b:")
  b = int(input())
  print("c =", get_hypotenuse(a,b))
+ print("S =", get_area(a,b))
