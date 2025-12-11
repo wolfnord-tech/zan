a4ca9cb1eff60f1de19e998907abe1cf8641ecaf
WolfNord <wolfnord.tech@yandex.ru>
2025-12-11 11:25:13 +0300

    Added comments

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 import math
+# Получить площадь прямоугольного треугольника
 def get_area(a, b):
  return a * b
+# Рассчитать гипотенузу
 def get_hypotenuse(a, b):
  return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 if __name__ == "__main__":
@@ -8,5 +10,5 @@ if __name__ == "__main__":
  a = int(input())
  print("Введите b:")
  b = int(input())
- print("c =", get_hypotenuse(a,b))
- print("S =", get_area(a,b))
+ print("c =", get_hypotenuse(a, b))
+ print("S =", get_area(a, b))
