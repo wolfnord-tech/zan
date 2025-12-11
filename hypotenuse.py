8f7e1981f0ba17c3c7b2c8e51c17a85b0289250e
WolfNord <wolfnord.tech@yandex.ru>
2025-12-11 11:05:28 +0300

    Changed hypotenuse.py

--- hypotenuse.py
+++ hypotenuse.py
@@ -2,6 +2,8 @@ import math
 def get_hypotenuse(a, b):
  return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 if __name__ == "__main__":
- a = 1
- b = 2
- print(get_hypotenuse(a,b))
+ print("Введите a:")
+ a = int(input())
+ print("Введите b:")
+ b = int(input())
+ print("c =", get_hypotenuse(a,b))
